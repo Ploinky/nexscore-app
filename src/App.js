43ea6a5c26a8b30ed2fe64761faf1ff6d65515de
Johannes Litger <mail@johannes-litger.de>
2022-05-10 12:09:02 +0200

    Added scoreboard component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 import React from 'react';
+import ScoreBoard from './components/scoreBoard/scoreBoard';
 
 function App() {
+  let players = [];
+
+  fetch('http://nexscore-env.eba-yxxpis3z.eu-central-1.elasticbeanstalk.com/players')
+      .then((data) => players = data);
+
   return (
-    <span>Hello world!</span>
+    <ScoreBoard players={players}/>
   );
 }
 
---
+++ src/components/scoreBoard/scoreBoard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function ScoreBoard({players}) {
+  return (<></>);
+};
+
+export default ScoreBoard;
+
+ScoreBoard.propTypes = {
+  players: PropTypes.array,
+};
