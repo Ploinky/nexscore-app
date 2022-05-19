abf0547273520fb89c63b7c0ffa8ef059c1d96af
Johannes Litger <mail@johannes-litger.de>
2022-05-19 14:33:30 +0200

    Added test for components

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,8 @@
-import {render, screen, waitFor} from '@testing-library/react';
+import {render} from '@testing-library/react';
 import React from 'react';
+import { act } from 'react-dom/test-utils';
 import App from './App';
-import ScoreBoard from './components/scoreBoard/scoreBoard';
+
 
 beforeEach(() => {
   fetch.resetMocks();
@@ -11,18 +12,4 @@ test('App is rendered without error', () => {
   fetch.mockResponseOnce(JSON.stringify([]));
 
   render(<App/>);
-});
-
-test('scoreboard', async () => {
-  const data = {players: [{puuid: '123', name: 'playername'},
-    {puuid: '321', name: 'nameplayer2'}], isLoaded: true};
-
-  render(<ScoreBoard data={data}/>);
-
-  await waitFor(() => screen.getByText('playername'));
-
-  const playername = screen.getByText(/playername/i);
-  expect(playername).toBeInTheDocument();
-  const nameplayer2 = screen.getByText(/nameplayer2/i);
-  expect(nameplayer2).toBeInTheDocument();
 });
